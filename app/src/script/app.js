e130d25c67b1d27c9eb42aa2a3a863de6c01f630
BOb <bobur@gmail.com>
2022-03-12 20:10:41 +0500

    pearl-release

--- app/src/script/app.js
+++ app/src/script/app.js
@@ -2,7 +2,9 @@ import menu from "./menu/menu.js"
 import Blur from './blur/blur.js'
 import slider from './slider/slider.js'
 
-slider()
+slider('.slider-btn1','.slider-img1')
+slider('.slider-btn2','.slider-img2')
+slider('.slider-btn3','.slider-images3')
 menu()
 
 var  blur = new Blur({
--- app/src/script/slider/slider.js
+++ app/src/script/slider/slider.js
@@ -1,20 +1,57 @@
-const slider = ()=> {
-    var btn = document.querySelectorAll('.menu__main-btn'),
-    images = document.querySelectorAll('.menu__main-img')
+const slider = (button, image)=> {
+    var btn = document.querySelectorAll(button),
+    images = document.querySelectorAll(image),
+    box = document.querySelectorAll('.menu__main-slider'),
+    activeSlide = 0,
+    b= 0;
 
-    var activeSlide = 0
+    var interval = setInterval(()=>{
+        b++
+        if (b > 3){
+            b = 0
+        }
+        imageChange(activeSlide,b)
+        removeActive()
+        btn[b].classList.add('active')
+        activeSlide= b
+    },5000)
 
-    
+
+    //перебор псевдо объектов
+    box.forEach(item =>{
+        item.addEventListener('mouseover' , ()=>{
+            clearInterval(interval)
+        })
+        item.addEventListener('mouseleave' , ()=>{
+            interval = setInterval(()=>{
+                b++
+                if (b > 3){
+                    b = 0
+                }
+                imageChange(activeSlide,b)
+                removeActive()
+                btn[b].classList.add('active')
+                activeSlide= b
+            },5000)
+        })
+    })
 
     btn.forEach((button,i) => {
         button.addEventListener('click' , ()=>{
-            removeActive()
-            addActive(button)
-            imageChange(activeSlide,i)
-            activeSlide= i
-            
+            slide(i, button)
         })
     })
+
+
+    //Функции
+
+    function slide(i, el){
+        removeActive()
+        addActive(el)
+        imageChange(activeSlide,i)
+        activeSlide= i
+    }
+
 
     function removeActive(){
         btn.forEach(btn =>btn.classList.remove('active'))
@@ -24,32 +61,42 @@ const slider = ()=> {
     }
 
     function imageChange(activeSlide,i){
-       if (activeSlide < i){
-           images[activeSlide].style = `
-           transition:0.5s;`
 
+       if (activeSlide < i){
+           
            for (let a = 0; a< i; a++){
                images[a].style=`
                transition:0;
-               right:100%`
-           }           
+               transform: translateX(-100%)`
+           }  
+           
+           
+           images[activeSlide].style = `
+           transition:0.5s;
+           transform: translateX(-100%)`
+
+
            images[i].style = `
-           right:0;
-           transition:0.5s;`
+           transition:0.5s;
+           transform: translateX(0);
+           `
 
 
        }else if (activeSlide > i){
-        images[activeSlide].style = `
-        transition:0.5s;
-        right:-100%;`
+        
 
         for (let   a =activeSlide; a> i; a--){
             images[a].style= ` 
             transition:0;
-            right:-100%`
+            transform: translateX(100%)`
         }
+        
+        images[activeSlide].style = `
+        transition:0.5s;
+        transform: translateX(100%)`
+
         images[i].style =`
-        right:0;
+        transform: translateX(0);
         transition:0.5s;`
        }
 
